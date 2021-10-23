718686fbd05edcdc8a65693eb70e7275d03174e7
DN-Vanguard <Domiltannguyen@outlook.com>
2021-10-23 16:36:27 -0700

    git portfoliopage and project were made.

---
+++ src/components/Project.js
@@ -0,0 +1,17 @@
+export default function Project( { title, imageName, liveURL, repoURL } ) {
+
+	return (
+		<div className="project-card">
+			<div className="project-image-btns">
+				<h4>{title}</h4>
+				<img src={`${process.env.PUBLIC_URL}/assets/${imageName}`} alt="Project"/>
+				<div className="project-btn-wrapper">
+					<a className="btn-left" href={liveURL} target="_blank" rel="noreferrer"
+						alt={`Go to ${title} Webpage`}>Live Site</a>
+					<a className="btn-right" href={repoURL} target="_blank" rel="noreferrer"
+						alt={`Go to ${title} Repo`}>Repository</a>
+				</div>
+			</div>
+		</div>
+	);
+}

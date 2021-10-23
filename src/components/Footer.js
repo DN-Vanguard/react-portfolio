2a284ca661ee2b8d172bea3a190c095173e6733a
DN-Vanguard <Domiltannguyen@outlook.com>
2021-10-23 16:42:37 -0700

    footer

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+export default function Footer() {
+
+	return (
+		<footer>
+			<a href="https://github.com/DN-Vanguard" target="_blank" rel="noreferrer">
+			</a>
+			<a href="https://www.linkedin.com/in/domiltannguyen/" target="_blank" rel="noreferrer">
+			</a>
+			<a href="https://github.com/DN-Vanguard/react-portfolio" target="_blank" rel="noreferrer">
+			</a>
+		</footer>
+	);
+}

a67ae0641de2e619982da16c15ffa2a7bc1a727f
DN-Vanguard <Domiltannguyen@outlook.com>
2021-10-23 16:37:34 -0700

    Navigation Panel

---
+++ src/components/Navigation.js
@@ -0,0 +1,37 @@
+const pageNames = [
+	{
+		id: 1,
+		name: 'About Me'
+	},
+	{
+		id: 2,
+		name: 'Portfolio'
+	},
+	{
+		id: 3,
+		name: 'Contact'
+	},
+	{
+		id: 4,
+		name: 'Resume'
+	}
+];
+
+export default function Navigation( { currentPage, handlePageChange } ) {
+
+	return (
+		<ul className="nav-tabs">
+			{pageNames.map( ( page ) => (
+				<li key={page.id}>
+					<a
+						href={'#' + page.name}
+						onClick={() => handlePageChange( page.name )}
+						className={currentPage === page.name ? 'nav-link active' : 'nav-link'}
+					>
+						{page.name}
+					</a>
+				</li>
+			) )}
+		</ul>
+	);
+}
